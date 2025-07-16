eabdb464428f6bec7d2f9ac916a86056bd8f0a94
Satvir Hundal <stormschool1@gmail.com>
2025-07-16 14:21:51 -0700

    Allowed for search bar to search by city

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash, current_app
 from werkzeug.security import generate_password_hash
-from flask_login import login_user, logout_user, login_required, current_user
-from models import User, get_user_by_username_or_email, get_user_by_id
+from flask_login import login_user, logout_user, login_required
+from models import get_user_by_username_or_email
 from utils import generate_confirmation_token, confirm_token
 from extensions import mail
 from flask_mail import Message
